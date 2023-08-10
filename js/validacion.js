d7c1e56f8db041bd6efbc34a80ee7be8476c3ede
gidgenf <gidgenf@hotmail.com>
2023-08-10 09:02:19 -0300

    Validación de acceso

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,24 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+
+regBtn.addEventListener('click', () => {
+
+    const nombre = document.getElementById('nombre').value
+    const apellido = document.getElementById('apellido').value
+    const email = document.getElementById('email').value
+    const contraseña = document.getElementById('password1').value
+    const repetircontraseña = document.getElementById('password2').value
+    const terminos = document.getElementById('terminos').checked
+
+    if (!nombre || !apellido || !email || !contraseña || !terminos || !repetircontraseña) {
+        showAlertError();
+    } else if (contraseña.length < 6 || contraseña !== repetircontraseña || !terminos) {
+        showAlertError();
+    } else {
+        showAlertSuccess();
+    }
+
+});
+
